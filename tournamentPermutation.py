9d43ccb7a5d9c988cbdad89e6c6390813daf5733
JSchmiegel <51242445+JSchmiegel@users.noreply.github.com>
2021-06-29 23:03:08 +0200

    updated script to calculate all permutation

--- tournamentPermutation.py
+++ tournamentPermutation.py
@@ -12,32 +12,36 @@ class game:
         self.loser = None
 
 
-    def play(self, overheadPrint):
-        i = randint(1,2)
-        if (i > 1):
+    def play(self, binary):
+        if (binary > 0):
             self.winner = self.contestant2
             self.loser = self.contestant1
         else:
             self.winner = self.contestant1
             self.loser = self.contestant2
-        tmp = " "
-        for i in range(overheadPrint * 20):
-            tmp += " "
-        # print(tmp + str(self.contestant1) + " vs. " + str(self.contestant2) + " --> " + str(self.winner))
 
-def playLayer(layer, overheadPrint):
+def playLayer(layer, level, binary, gameNumber):
     global results
+    
     newLayer = list()
     for g in layer:
+        winner = int(str(binary)[-1])
+        g.play(winner)
         tmpstr = str(g.contestant1) + " vs. " + str(g.contestant2)
-        results["games"].append({"contestant": tmpstr, "level": overheadPrint, "winner": g.contestant1})
-        g.play(overheadPrint)
+        results["games"].append({
+            "contestant": tmpstr, 
+            "level": level, 
+            "winner": g.winner, 
+            "gameNumber": int(gameNumber)
+        })
+        binary = bin(int(binary, 2) >> 1)
     if len(layer) > 1:
         i = 0
-        while i < len(layer):
+        while i < len(layer)-1:
             newLayer.append(game(layer[i].winner,layer[i+1].winner))
             i = i + 2
-        playLayer(newLayer, overheadPrint + 1)
+        playLayer(newLayer, level + 1, binary, gameNumber)
+    newLayer.clear()
 
 #Uefa Euro 2016 (Round of 16)
 # playerslist = [
@@ -59,39 +63,50 @@ def playLayer(layer, overheadPrint):
 #     "Island"
 # ]
 #Uefa Euro 2016 (Round of 8)
-# playerslist = [
-#     "Polen",
-#     "Portugal",
-#     "Wales",
-#     "Belgien",
-#     "Deutschland",
-#     "Italien",
-#     "Frankreich",
-#     "Island"
-# ]
-#Uefa Euro 2016 (Round of 4)
 playerslist = [
+    "Polen",
     "Portugal",
     "Wales",
+    "Belgien",
     "Deutschland",
-    "Frankreich"
+    "Italien",
+    "Frankreich",
+    "Island"
 ]
+#Uefa Euro 2016 (Round of 4)
+# playerslist = [
+#     "Portugal",
+#     "Wales",
+#     "Deutschland",
+#     "Frankreich"
+# ]
 
 ##main
 games = list()
-i = 0
-while i < len(playerslist):
-    games.append(game(playerslist[i], playerslist[i+1]))
-    i = i + 2
-playLayer(games, 0)
 
+#calculate all options (parsed as binary number) (only semifinals!!)
+probability = list()
+numberOfPermutation = 2**(len(playerslist)-1)
+for i in range(numberOfPermutation):
+    probability.append(bin(i))
+
+j = 0
+for el in probability:
+    games.clear()
+    i = 0
+    while i < len(playerslist):
+        games.append(game(playerslist[i], playerslist[i+1]))
+        i = i + 2
+    playLayer(games, 0, el, j)
+    j = j+1
 
 #output
+resultsSort = sorted(results["games"], key=lambda x : x["gameNumber"], reverse=False)
 with open("permutations.json", "w") as file:
-    json.dump(results["games"], file)
-for entry in results["games"]:
+    json.dump(resultsSort, file)
+for entry in resultsSort:
     tmp = ""
     for i in range(entry["level"] * 20):
         tmp += " "
-    print(tmp + entry["contestant"] + " --> " + entry["winner"])
+    print(tmp + str(entry["gameNumber"]) +  " " + entry["contestant"] + " --> " + entry["winner"])
 print()
